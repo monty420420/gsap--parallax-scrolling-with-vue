c4a0b6db1e19684b67384cf9b81633c00925cd68
Muntaeyeon <72345833+monty420420@users.noreply.github.com>
2024-03-13 17:02:08 +0900

    scss 파일로 스타일 이동

--- src/components/element/Button.vue
+++ src/components/element/Button.vue
@@ -1,21 +1,6 @@
 <template>
-    <button>
+    <button type="button">
         <slot />
     </button>
 </template>
-
-<style scoped>
-    button{
-        outline: none;
-        cursor: pointer;
-        width: 28px;
-        height: 28px;
-        border-radius: 33px;
-        border: 1px solid black;
-        background: black;
-    }
-</style>
-
-<script>
-</script>
 
