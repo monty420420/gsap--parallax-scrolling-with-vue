cbc42be7f8919e5f422c51286c124eecbfb16d37
Muntaeyeon <72345833+monty420420@users.noreply.github.com>
2024-03-14 15:49:19 +0900

    redpoint 이벤트

--- src/components/pages/map.vue
+++ src/components/pages/map.vue
@@ -3,7 +3,7 @@
     <div class="open-wrap">
       <Button
         class="open-map"
-        @click="isModal = true"
+        @click="(isModal = true)"
         @mouseenter="buttonHover(true, 'open-map')"
         @mouseleave="buttonHover(false, 'open-map')"
       >
@@ -15,13 +15,13 @@
       <div v-if="isModal" class="map-modal">
         <img class="modal-background" src="/assets/image/map.png" alt="" />
         <ul class="red-point">
-          <li>
+          <li @click="togglePoint(1)">
             <span class="blind">red point</span>
           </li>
-          <li>
+          <li @click="togglePoint(2)" v-if="pointVisible > 0">
             <span class="blind">red point</span>
           </li>
-          <li>
+          <li v-if="pointVisible > 1">
             <span class="blind">red point</span>
           </li>
         </ul>
@@ -61,11 +61,111 @@ export default {
       });
     });
 
+    const pointVisible = ref(0);
+
+    const togglePoint = (index) => {
+      if( pointVisible.value != index ){
+        ++pointVisible.value;
+      }
+    };
+
     return {
       isModal,
       buttonHover,
+      togglePoint,
+      pointVisible,
     };
   },
 };
 </script>
+<style lang="scss">
+.open-wrap {
+  position: fixed;
+  left: 10%;
+  top: auto;
+  bottom: 3%;
+  margin-bottom: 40px;
+  width: 28px;
+  height: 28px;
+  border: 1px solid black;
+  border-radius: 33px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .open-map {
+    overflow: hidden;
+    img {
+      padding-top: 5px;
+      width: 16px;
+      height: 16px;
+    }
+    img:nth-child(2) {
+      padding-top: 2px;
+    }
+  }
+}
+.map-modal {
+  z-index: 3;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  height: 100%;
+  transform: translate(-50%, -50%);
+  background-image: url(/assets/image/map-background.jpg);
+  background-position: 50% 50%;
+  background-size: cover;
+  .modal-background {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+  .red-point {
+    li {
+      width: 15px;
+      height: 15px;
+      border-radius: 100px;
+      background: #6c0e11;
+      position: fixed;
+      cursor: pointer;
+      animation: redSpread 2s infinite ease-out;
+    }
+    li:nth-of-type(1) {
+      top: 48%;
+      left: 48%;
+    }
+    li:nth-of-type(2) {
+      top: 20%;
+      left: 20%;
+    }
+    li:nth-of-type(3) {
+      top: 22%;
+      left: 78%;
+    }
+  }
+  .close-map {
+    position: fixed;
+    overflow: hidden;
+    top: 12%;
+    right: 10%;
+    img {
+      padding-top: 5px;
+      width: 16px;
+      height: 16px;
+    }
+    img:nth-child(2) {
+      padding-top: 2px;
+    }
+  }
+}
+
+@keyframes redSpread {
+  0% {
+    box-shadow: 0px 0px 0px 0px rgba(100, 0, 0, 1);
+  }
+  100% {
+    box-shadow: 0px 0px 0px 70px rgba(88, 0, 0, 0);
+  }
+}
+</style>
 
